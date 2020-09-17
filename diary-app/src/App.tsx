6b99dffc1f9b6976390805d2254bcb00dd382d38
David Wong <davidwong369@outlook.com>
2020-09-17 21:37:44 +1200

    Add project

--- diary-app/src/App.tsx
+++ diary-app/src/App.tsx
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import ToolBar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button'
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+import DiaryEntryCard from './Components/diaryEntryCard/diaryEntryCard';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import './App.css'
+import logo from './companyLogo.png'
+
+const useStyles = makeStyles({
+  topBar:  {
+    background: '#29B6F6',
+  },
+
+});
+
 
 function App() {
+
+  const classes = useStyles();
+  
+  const [open, setOpen] = useState(false)
+  const [diaryEntries, setdiaryEntries] = useState([])
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const formSubmit= () => {
+    const titleElement = document.getElementById("title") as HTMLInputElement;
+    const entryElement = document.getElementById("entry") as HTMLInputElement;
+
+    const body = {
+      title: titleElement.value,
+      entry: entryElement.value
+    }
+
+    console.log(body)
+
+    fetch(`https://dwondiaryAPI.azurewebsites.net/api/Diaries`,{
+    headers: {
+      Accept: "application/json",
+      'Content-Type': 'application/json'
+    },
+    method:'POST',
+    body:JSON.stringify(body),
+    }).then(res => console.log(res))
+    setOpen(false);
+
+  }
+
+  useEffect(() => {
+    fetch('https://dwondiaryAPI.azurewebsites.net/api/Diaries')
+    .then(response => response.json())
+    .then(json => setdiaryEntries(json))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static" className={classes.topBar} id="topBar">
+        <ToolBar>
+          <img src={logo} id="logo"></img>
+          <Typography variant="h2" id="companyName">
+            Diary Write
+          </Typography>
+          <Button onClick={handleOpen}  id="newEntryButton" >Add Diary Entry</Button>
+        </ToolBar>
+      </AppBar>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New Diary Entry</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Write in the fields below to add a new diary entry
+            </DialogContentText>
+
+            <TextField 
+              id="title"
+              margin="dense"
+              label="Title"
+              type="text"
+              inputProps={{
+                  maxLength: 50,
+                }}
+              fullWidth
+            />
+
+            <TextField 
+              id="entry"
+              margin="dense"
+              label="Entry"
+              type="text"
+              multiline
+              fullWidth
+              inputProps={{
+                  maxLength: 255,
+                }}
+            />
+
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button onClick={formSubmit}>
+              Submit
+            </Button> 
+          </DialogActions>
+        </Dialog>
+
+      <Container id="container">
+        {diaryEntries.map(diaryEntry => {
+          return <DiaryEntryCard diaryEntry = {diaryEntry}/>
+        })}
+      </Container>
+ 
+
+    
+
     </div>
   );
 }
 
 export default App;
+
+
---
+++ diary-app/src/Components/diaryEntryCard/diaryEntryCard.tsx
@@ -0,0 +1,169 @@
+import React, { useEffect, useState } from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+import  './diaryEntryCard.css';
+
+import CreateIcon from '@material-ui/icons/Create';
+import DeleteIcon from '@material-ui/icons/Delete';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseIcon from '@material-ui/icons/Pause';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import { useSpeechSynthesis, cancel } from 'react-speech-kit';
+
+
+const useStyles = makeStyles({
+    diaryCard: {
+      maxWidth: 1200,
+    },
+    binButton: {
+        color: 'red'
+    }
+
+  });
+
+function DiaryEntryCard ({diaryEntry}) {
+
+    const { speak,cancel } = useSpeechSynthesis();
+
+    const classes = useStyles();
+    const [open, setOpen] = useState(false)
+    const [state, setState] = useState({diaryEntry})
+    const [title, setTitle] = useState(diaryEntry.title)
+    const [entry, setEntry] = useState(diaryEntry.entry)
+
+    console.log(title)
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const formSubmit = () => {
+        const titleElement = document.getElementById("title") as HTMLInputElement;
+        const entryElement = document.getElementById("entry") as HTMLInputElement;
+
+        const body = {
+            id: diaryEntry.id,
+            title: titleElement.value,
+            entry: entryElement.value
+        }
+        console.log(body)
+       
+        fetch(`https://dwondiaryAPI.azurewebsites.net/api/Diaries/${diaryEntry.id}`,{
+        headers: {
+            Accept: "application/json",
+            'Content-Type': 'application/json'
+    
+        },
+        method:'PUT',
+        body:JSON.stringify(body),
+        }).then(res => console.log(res))
+        setOpen(false);
+    }
+
+    const deleteEntry = () => {
+        fetch(`https://dwondiaryAPI.azurewebsites.net/api/Diaries/${diaryEntry.id}`,{
+            method:'DELETE'
+        })
+
+
+    }
+    return(
+        <div>
+            
+            <Card className={classes.diaryCard} id="diaryCard">
+                <CardContent>
+                    <h1 id="displayedTitle">{title}</h1>
+                    <p id="displayedEntry">{entry}</p>
+                </CardContent>
+
+                <CardActions>
+                    <ButtonGroup>
+                        <IconButton onClick = {handleOpen} color="primary">
+                            <CreateIcon/>
+                        </IconButton>
+                        <IconButton onClick = {deleteEntry} className={classes.binButton}>
+                            <DeleteIcon/>
+                        </IconButton>
+                        <IconButton onClick ={() => speak({ text:entry})}>
+                            <PlayCircleOutlineIcon/>
+                        </IconButton>
+
+                        <IconButton onClick ={cancel}>
+                            <PauseIcon/>
+                        </IconButton>
+    
+                    </ButtonGroup>
+                </CardActions>
+
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Edit Diary Entry</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Write in the fields below to edit the diary entry
+                        </DialogContentText>
+
+                        <TextField 
+                        defaultValue={diaryEntry.title}
+                        onChange={e =>setTitle(e.target.value)}
+
+                        id="title"
+                        margin="dense"
+                        label="Title"
+                        type="text"
+                        inputProps={{
+                            maxLength: 50,
+                          }}
+                        fullWidth
+                    
+                        />
+
+                        <TextField 
+                        defaultValue={diaryEntry.entry}
+                        onChange={e =>setEntry( e.target.value)}
+
+                        id="entry"
+                        margin="dense"
+                        label="Entry"
+                        type="text"
+                        multiline
+                        fullWidth
+
+                        inputProps={{
+                            maxLength: 255,
+                          }}
+        
+                        
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button onClick={formSubmit}>
+                            Submit Edit
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Card>
+        </div>
+    )
+}
+
+export default DiaryEntryCard;
